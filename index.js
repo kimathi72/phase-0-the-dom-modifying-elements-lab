e2203768f050ff6591c0bea5da053b72a74b22a6
roy <kimathiwaweru@gmail.com>
2023-03-04 01:27:37 +0300

    done with assignment

--- index.js
+++ index.js
@@ -7,23 +7,3 @@ newHeader.id = "victory"
 const yourName = `Roy kimathi`
 newHeader.textContent = `${yourName} is the champion` 
 
-
-// const element = document.createElement("div");
-// document.body.append(element);
-// const ul = document.createElement("ul");
-
-// for (let i=0; i < 3; i++) {
-//     const li = document.createElement("li");
-//     li.textContent = (i + 1).toString();
-//     ul.append(li);
-// }
-// element.append(ul);
-// element.style.height = "300px";
-// element.style.backgroundColor = "#27647B";
-// element.textContent = "You've changed what's on the screen!";
-// element.style.fontSize = "24px";
-// element.style.marginLeft = "30px";
-// element.style.lineHeight = 10;
-// element.className = "pet-listing dog";
-// element.classList.remove("dog");
-// element.classList.add("cat", "sale");
